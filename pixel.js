3bc995b9cc995ff33efd3b44120357bf14159a0f
Joshua Lerner <joshualerner@Joshuas-MacBook-Pro.local>
2017-02-12 21:39:52 -0700

    pixelsgit add .

--- pixel.js
+++ pixel.js
@@ -4,7 +4,7 @@ document.addEventListener('DOMContentLoaded', function(){
 let body = document.getElementsByTagName('body')[0]
 let header = document.createElement('header')
    body.appendChild(header)
-   header.innerText = "PIXEL PIXEL PIXEL"
+   header.innerText = "PIXEL FUN TIME!"
 let pSection = document.createElement('section')
    body.appendChild(pSection)
 let footer = document.createElement('footer')
@@ -12,11 +12,15 @@ let footer = document.createElement('footer')
    let fDiv = document.createElement ('div')
    footer.appendChild(fDiv)
    fDiv.setAttribute ('id', 'pal')
+   let clearDiv = document.createElement ('div')
+   footer.appendChild(clearDiv)
+   clearDiv.setAttribute('id', 'clear')
+   clearDiv.innerText = "Palette Cleanser"
 let pixels = document.getElementsByClassName('pixel')
 
 //make pixels
 function makePixels() {
-   for (var i = 0; i < 1988; i++) {
+   for (var i = 0; i < 2010; i++) {
       let div = document.createElement('div')
       pSection.appendChild(div)
       div.className = 'pixel'
@@ -31,14 +35,12 @@ function choosey() {
       pColor = color.id;
    }
   }
-//mouse down
-//mouse over
-//mouse up
+
 document.addEventListener('click', choosey)
 // document.addEventListener('mousedown', choosey)
 
 //colors in palette
-colorArray = ['#000000', '#573323', 'rgb(198, 113, 36)', 'orange', 'rgb(144, 237, 25)','green', 'rgb(30, 213, 185)', 'rgb(25, 142, 200)', 'rgb(14, 55, 136)', 'white']
+colorArray = ['#000000', '#573323', 'rgb(198, 113, 36)', 'orange', '#fcfa21', 'rgb(144, 237, 25)','rgb(25, 182, 78)', 'rgb(30, 213, 185)', 'rgb(25, 142, 200)', 'rgb(14, 55, 136)', '#6a5ab8', '#7e37b5', '#d70cbd', 'rgb(189, 6, 83)', 'white']
 pColor = colorArray[0];
 
 //change pixel into certain color on click
@@ -63,4 +65,15 @@ for (var color of colorArray){
 
    div.style.backgroundColor = (color)
 }
+
+function cleary() {
+   clear = event.target
+   let pxls = document.getElementsByClassName('pixel')
+   if (clear.id == 'clear'){
+      for(let p of pxls)
+         p.style.backgroundColor = "white"
+   }
+}
+document.addEventListener('click', cleary)
+
 })
